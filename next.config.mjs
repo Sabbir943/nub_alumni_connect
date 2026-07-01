410298ae4670723e40d4c179c146bf41b26b8c90
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-07-01 18:54:21 +0600

    added ksley pakage

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+   serverExternalPackages:['@better-auth/kysely-adapter','kysely'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allows all HTTPS domains
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // Allows all HTTP domains
+      },
+    ],
+  },
   reactCompiler: true,
 };
 
