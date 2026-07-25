1ed0a6372dd6b0b6c6e885055e93de4fa59a0590
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2026-07-25 23:41:56 +0600

    done by loading.js

---
+++ src/app/dashboard/alumni/layout.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardSplash from '@/component/DashboardSplash';
+
+const AlumniLayout = ({ children }) => {
+  return <DashboardSplash>{children}</DashboardSplash>;
+};
+
+export default AlumniLayout;
